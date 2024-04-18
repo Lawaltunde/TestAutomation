beaae6c2c358e3f9f020d5882329ea77774155ba
Lawal Tunde <hlawal@mycambrian.ca>
2024-04-18 17:50:44 -0400

    Last test added to validate the login button

--- TestAutomation/TestAutomation/TestAutomationActivity.cs
+++ TestAutomation/TestAutomation/TestAutomationActivity.cs
@@ -11,21 +11,35 @@ namespace TestAutomation
         [TestMethod]
         public void TestMethod1()
         {
-            // Instantiating webdriver
-            IWebDriver webDriver = new ChromeDriver();
 
-            // Accessing the site url
-            webDriver.Navigate().GoToUrl("https://hackernoon.com/");
+            try
+            {
+                // Instantiating webdriver
+                IWebDriver webDriver = new ChromeDriver();
+
+                // Accessing the site url
+                webDriver.Navigate().GoToUrl("https://hackernoon.com/");
 
 
-            //First Test: validating the login link
-            webDriver.FindElement(By.ClassName("sc-b3d23cc4-0")).Click();
+                //First Test: validating the login link
+                webDriver.FindElement(By.ClassName("sc-b3d23cc4-0")).Click();
 
-            //Second Test: Validating the email address
-            webDriver.FindElement(By.Id("email")).SendKeys("A00276443@mycambrian.ca");
+                //Second Test: Validating the email address
+                webDriver.FindElement(By.Id("email")).SendKeys("A00276443@mycambrian.ca");
 
-            //Third Test: Validating the password of the website
-            webDriver.FindElement(By.Id("password")).SendKey("Password123");
+                //Third Test: Validating the password of the website
+                webDriver.FindElement(By.Id("password")).SendKeys("Password123");
+
+                //Fourth Test: Validating the login button class="sc-629a08f7-5 kmfnwX"
+                webDriver.FindElement(By.ClassName("sc-629a08f7-5")).Click();
+
+            }
+            catch(Exception ex) {
+               Console.WriteLine(ex.Message);
+            }
+           
+           
+
 
 
         }
