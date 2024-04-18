3be9464c1ce82a1074b096482f57828f96dd59f0
Lawal Tunde <hlawal@mycambrian.ca>
2024-04-18 16:53:27 -0400

    First test added : this test is validating the login link

---
+++ TestAutomation/TestAutomation/TestAutomationActivity.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace TestAutomation
+{
+    [TestClass]
+    public class TestAutomationActivity
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Instantiating webdriver
+            IWebDriver webDriver = new ChromeDriver();
+
+            // Accessing the site url
+            webDriver.Navigate().GoToUrl("https://hackernoon.com/");
+
+
+            //First Test:validating the login link
+            webDriver.FindElement(By.ClassName("sc-b3d23cc4-0 biyRsJ")).Click();
+
+        }
+    }
+}
